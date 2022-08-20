fb15d8c891d0a8c7f47216ad750dfd567b46e452
veevekdave <veevekd401@gmail.com>
2022-08-19 23:14:17 -0400

    Created Robot Component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
+import Card from './card'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Card name='Veevek' id="veevek" email = "Veevek" />
   </React.StrictMode>
 );                                                                                                                                
 
---
+++ src/robots.js
@@ -0,0 +1,8 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Veevek Dave',
+    username: 'veevekdave',
+    email: 'veevekd401@gmail.com'
+  }
+]
